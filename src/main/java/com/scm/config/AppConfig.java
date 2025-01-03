8d3f1f7e474e604f077549222dfa3c639ea5ab69
Aniket <saniketchavhan123@gmail.com>
2025-01-03 21:50:43 +0530

    added environmental variables in application.properties file

--- src/main/java/com/scm/config/AppConfig.java
+++ src/main/java/com/scm/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.scm.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
@@ -10,23 +12,47 @@ import com.cloudinary.utils.ObjectUtils;
 @Configuration
 public class AppConfig
 {
-	@Value("${cloudinary.cloud.name}")				// the details are fetch from environment variable
-	private String cloudName ;
-	
-	@Value("${cloudinary.api.key}")
-	private String apiKey ;
-	
-	@Value("${cloudinary.api.secret}")
-	private String apiSecret ;
 	
 	
-	@Bean
-	public Cloudinary cloudinary()
-	{
-		
-		return new Cloudinary(ObjectUtils.asMap("cloud_name",cloudName,
-				"api_key",apiKey, 
-				"api_secret", apiSecret )
-				);
-	}
+	 @Autowired
+	    private Environment environment;
+
+	    @Bean
+	    public Cloudinary cloudinary() {
+	        String cloudName = environment.getProperty("cloudinary.cloud.name");
+	        String apiKey = environment.getProperty("cloudinary.api.key");
+	        String apiSecret = environment.getProperty("cloudinary.api.secret");
+
+	        return new Cloudinary(ObjectUtils.asMap(
+	                "cloud_name", cloudName,
+	                "api_key", apiKey,
+	                "api_secret", apiSecret
+	        ));
+	
+	    }
+	        
+	        
+	
+	
+	
+	
+//	@Value("${cloudinary.cloud.name}")				// the details are fetch from environment variable
+//	private String cloudName ;
+//	
+//	@Value("${cloudinary.api.key}")
+//	private String apiKey ;
+//	
+//	@Value("${cloudinary.api.secret}")
+//	private String apiSecret ;
+//	
+//	
+//	@Bean
+//	public Cloudinary cloudinary()
+//	{
+//		
+//		return new Cloudinary(ObjectUtils.asMap("cloud_name",cloudName,
+//				"api_key",apiKey, 
+//				"api_secret", apiSecret )
+//				);
+//	}
 }
